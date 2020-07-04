9819fe04eedc7f4a96a2c30465739e448c7bd655
Anderson Kruel <anderson.kruel@gmail.com>
2020-07-04 20:23:15 -0300

    concluindo desafio;

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
-
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
-    }
+    uploadedFiles.forEach(async file => {
+      data.append('file', file.file, file.name);
+      try {
+        const response = await api.post('/transactions/import', data);
+        console.log('Sucesso!', response.status);
+      } catch (err) {
+        console.log(err.response.error);
+      }
+    });
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const fileProps: FileProps[] = [];
+
+    files.forEach(file => {
+      fileProps.push({
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      });
+    });
+
+    setUploadedFiles([...uploadedFiles, ...fileProps]);
   }
 
   return (
